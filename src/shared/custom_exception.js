a10696d8248282abb181117cffe5e3537953219b
denu <nissgonzalezm@gmail.com>
2025-09-06 14:33:11 -0300

    refactor: remove unnecessary whitespace and clean up CustomException constructor

--- src/shared/custom_exception.js
+++ src/shared/custom_exception.js
@@ -1,13 +1,11 @@
 import Status from "./status.js";
 
-
-
 export class CustomException extends Error {
     constructor({ message, status = Status.internalServerError }) {
         super(message);
         this.name = "CustomException";
         this.status = status;
-        if (Error.captureStackTrace) Error.captureStackTrace(this, CustomException);
+
     }
 
     toJSON() {
