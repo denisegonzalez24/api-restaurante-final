846f6b931843d5e5dd40c3a81cd734866a7d889f
denu <nissgonzalezm@gmail.com>
2025-09-08 16:34:43 -0300

    errorHandler refaccion

--- src/presentation/middleware/handler_error.js
+++ src/presentation/middleware/handler_error.js
@@ -2,17 +2,17 @@ import { ApiError } from "../../shared/ApiError.js";
 import Status from "../../shared/status.js";
 
 export function errorHandler(err, req, res, next) {
-    // Sequelize unique -> 409
+
     if (err?.name === 'SequelizeUniqueConstraintError') {
-        return res.status(409).json({ message: 'Ya existe un plato con ese nombre' });
+        return res.status(Status.conflict).json({ message: 'Ya existe un plato con ese nombre' });
     }
-    // Errores de dominio
+
     if (err instanceof ApiError) {
         return res.status(err.statusCode).json({ message: err.message ?? null });
     }
-    // Fallback
+
     console.error(err);
-    return res.status(500).json({ message: 'Error interno' });
+    return res.status(Status.internalServerError).json({ message: 'Error interno' });
 }
 
 
